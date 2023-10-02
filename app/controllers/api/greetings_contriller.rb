e9222b61afd5416f592c486a1acb9499255d54e9
Enoisong <enobongitiaba988@gmail.com>
2023-10-02 16:46:02 -0700

    first commit

---
+++ app/controllers/api/greetings_contriller.rb
@@ -0,0 +1,7 @@
+class Api::GreetingsController < ApplicationController
+    def random_greeting
+      random_greeting = Message.order('RANDOM()').first
+      render json: { greeting: random_greeting.content }
+    end
+  end
+  
